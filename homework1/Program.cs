a9f0ddd87dd96bcd7a6153531f8adcf8c339bf2b
Andrey <andrei_uskov@mail.ru>
2023-04-26 21:24:10 +0300

    Первая задача

---
+++ homework1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число:");
+int number1 = int.Parse (Console.ReadLine());
+Console.WriteLine ("Введите второе число:");
+int number2 = int.Parse (Console.ReadLine());
+int max = number1;
+int min = number2;
+if (number1 < number2) max = number2;
+if (max == number2) min = number1;
+Console.Write("Наибольшим числом является: ");
+Console.Write (max);
+Console.Write(", а наименьшим числом является: ");
+Console.Write (min);
