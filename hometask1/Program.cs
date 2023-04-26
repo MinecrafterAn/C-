db31b880d7edd7f1d1d4ce8692eb997d14e51b8d
Andrey <andrei_uskov@mail.ru>
2023-04-26 22:29:52 +0300

    Первая задача

--- hometask1/Program.cs
+++ hometask1/Program.cs
@@ -4,9 +4,10 @@ Console.WriteLine ("Введите второе число:");
 int number2 = int.Parse (Console.ReadLine());
 int max = number1;
 int min = number2;
-if (number1 < number2) max = number2;
-if (max == number2) min = number1;
-Console.Write("Наибольшим числом является: ");
-Console.Write (max);
-Console.Write(", а наименьшим числом является: ");
-Console.Write (min);
+if (number1 < number2)
+{
+    max = number2;
+    min = number1;
+}
+Console.Write("Наибольшим числом является: " + max);
+Console.Write(", а наименьшим числом является: " + min);
