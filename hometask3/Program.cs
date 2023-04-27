8e3fd1ff3366f32fa2464a9af4f4df2f0ec97f33
Andrey <andrei_uskov@mail.ru>
2023-04-27 19:42:45 +0300

    Решил третью задачу

---
+++ hometask3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse (Console.ReadLine());
+if (number %2 == 0)
+{
+    Console.WriteLine ("Введеное вами число является четным");
+}
+else 
+{
+    Console.WriteLine ("Введеное вами число является нечетным");
+}
