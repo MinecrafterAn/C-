849001125c5483d9b97dea70a4723a22a8dd3aa2
Andrey <andrei_uskov@mail.ru>
2023-04-27 20:26:38 +0300

    Сделал четвертую задачу

---
+++ hometask4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse (Console.ReadLine());
+int number1 = 1;
+while (number1 < number + 1)
+{
+    if (number1 %2 == 0)
+    Console.Write (number1 + (","));
+    number1 +=1;
+}
