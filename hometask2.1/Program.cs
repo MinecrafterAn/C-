89089c683a879897f98bbb81197d15fbfdc511fa
Andrey <andrei_uskov@mail.ru>
2023-05-08 21:48:03 +0300

    первая задача ко второму семинару

---
+++ hometask2.1/Program.cs
@@ -0,0 +1,4 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine("Случайное число: " + number);
+int seconddigit = number%100 / 10;
+Console.WriteLine ("Вторая цифра данного числа: " + seconddigit);
