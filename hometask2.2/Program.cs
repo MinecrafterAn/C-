bd6923e4aa7d44db577bc30312f6f7dfc34e7463
Andrey <andrei_uskov@mail.ru>
2023-05-14 21:56:44 +0300

    доделал домашку по второму семинару

---
+++ hometask2.2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine ("Введите число: ");
+int number = int.Parse (Console.ReadLine());
+int step = 1;
+int checkstep = number;
+while (checkstep > 9)
+{
+    checkstep = checkstep / 10;
+    step = step + 1;
+}
+if (step < 3)
+{
+    Console.WriteLine("Введенное вами число имеет меньше трех цифр, повторите ввод: ");
+}
+else
+{
+while (step > 3)
+{
+    number = number/10;
+    step = step - 1;
+}
+number = number%10;
+Console.WriteLine (number);
+}
---
+++ hometask2.3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите номер дня недели от 1 до 7: ");
+int numberday = int.Parse (Console.ReadLine());
+if (numberday < 6)
+{
+    Console.WriteLine("К сожалению это будний день...");
+}
+else
+{
+    Console.WriteLine("Поздравляем! Это выходной!");
+}
